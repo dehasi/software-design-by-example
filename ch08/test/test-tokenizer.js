48369e3af271272fe40d77dd1d26f952b8356e00
Ravil Galeyev <dehasi@yandex.ru>
2022-12-27 20:15:45 +0100

    run tst

--- ch08/test/test-tokenizer.js
+++ ch08/test/test-tokenizer.js
@@ -1,11 +1,11 @@
 import assert from "assert";
 
-import tokenize from "./tokenizer.js";
+import tokenize from "../tokenizer.js";
 
 describe('tokenizes correctly', async () => {
     it('tokenizes a single character', () => {
         assert.deepStrictEqual(tokenize('a'), [
-            { kind: 'Lit', loc: 0, value: 'b' }
+            { kind: 'Lit', loc: 0, value: 'a' }
         ])
     })
 });
